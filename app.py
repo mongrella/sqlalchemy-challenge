29140de02de3494f8bf1ef24e50775066a4b26fd
lucille bluth <melissa.mongrella@northwestern.edu>
2021-04-23 14:39:37 -0400

    updated session and prcp data

--- app.py
+++ app.py
@@ -36,27 +36,64 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-
-#Home page.
-
-
-
-
-
+#Home page
 #List all routes that are available.
-
-
+#NEED MORE WORK ON THIS
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/START<br/>"
+        f"/api/v1.0/STARTEND<br/>"
+    )
 
 
 #/api/v1.0/precipitation
 #Convert the query results to a dictionary using date as the key and prcp as the value.
 #Return the JSON representation of your dictionary.
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+# Create our session (link) from Python to the DB
+    session = Session(engine)
+   
+    """Return a list of all precipitation"""
+    # Query all precipitation
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= "2016-08-24").all()
+    session.close()
 
+# Create a dictionary from the list of precipitation
 
+    all_precipitation = []
+    for date,prcp in results:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"] = prcp
 
+        all_precipitation.append(precipitation_dict)
+
+    return jsonify(all_precipitation)
 
 #/api/v1.0/stations
-#Return a JSON list of stations from the dataset.
+#Return a JSON list of stations from the dataset in desscending order.
+@app.route("/api/v1.0/stations")
+def stations():
+# Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all stations"""
+    # Query all stations
+    results = session.query(Station.station).order_by(Station.station).all()
+    session.close()
+
+    # Convert list of tuples into normal list
+    stations_list = list(np.travel(results))
+
+    return jsonify(all_precipitation)
+
 
 
 
@@ -64,7 +101,7 @@ app = Flask(__name__)
 #Query the dates and temperature observations of the most active station 
 # for the last year of data.
 #Return a JSON list of temperature observations (TOBS) for the previous year.
-
+@app.route("/api/v1.0/tobs")
 
 
 
